59a9935849861b809923f0207d43a66c5910a2a6
andrew woods <andrewwoods88@gmail.com>
2017-04-03 17:46:47 +0000

    added comments to articles.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base 
-
+#makes sure when you create an article it contains a title and a description.
+#before a new article hits the database this ensures that it has an title, description
 validates :title, presence: true, length: {minimum: 3, maximum: 50}
 
 validates :description, presence: true, length: {minimum: 10, maximum: 300}
